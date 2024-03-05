9b6b923c9ab1d6b3b4bd2aaf6554238a61d24da8
camilo gonzalez <camilo50075@gmail.com>
2024-03-05 18:50:24 -0500

    created proyect

---
+++ src/test/java/eam/apps/Parcial1ApplicationTests.java
@@ -0,0 +1,13 @@
+package eam.apps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcial1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
